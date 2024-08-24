eb00acbed315753c237c29720d83cc9afa68e25d
camacholucas <lucasdasilvacamacho@gmail.com>
2024-08-24 18:40:32 -0300

    objeto

--- aulas/2-tipos-primitivos.js
+++ aulas/2-tipos-primitivos.js
@@ -1,5 +1,9 @@
 let nome = 'lucas' //string literal
 let idade = 25; //number literal
 let estaAprovado = true; //(true or false)boolean, ajuda a validar regras
-let sobrenome = undefined; //Undefined
+let sobrenome = undefined; //Undefined, 
 let corSelecionado = null; //Redefinir um valor
+
+console.log(typeof corSelecionado)
+
+//
---
+++ aulas/3-objeto.js
@@ -0,0 +1,11 @@
+//Objeto é uma forma de representar coisas ou conceitos no contexto de um programa
+let pessoa = {
+    nome: 'Lucas',
+    idade: 28,
+    estaAprovado: true,
+    sobrenome: 'Camacho',
+    telefone: '+55(21)98170-9696',
+    nacionalidade: 'brasileira'
+};
+
+console.log(pessoa);
