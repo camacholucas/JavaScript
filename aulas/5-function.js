76d6f741212db734f9a455f490d6a4c33b30415f
camacholucas <lucasdasilvacamacho@gmail.com>
2024-08-24 18:54:26 -0300

    function

---
+++ aulas/5-function.js
@@ -0,0 +1,12 @@
+//Verbo + Substantivo
+
+let corSite = "azul";
+
+function resetaCor(cor,tonalidade){
+    corSite = cor + tonalidade;
+};
+
+console.log(corSite);
+resetaCor("Amarelo"," escuro");
+console.log(corSite);
+
