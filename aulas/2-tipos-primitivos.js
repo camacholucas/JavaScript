29b77c96ee20135090822fe2c5273a43e7f201b1
camacholucas <lucasdasilvacamacho@gmail.com>
2024-08-24 16:47:42 -0300

    tipos primitivos

--- aulas/2-tipos-primitivos.js
+++ aulas/2-tipos-primitivos.js
@@ -1,2 +1,5 @@
 let nome = 'lucas' //string literal
 let idade = 25; //number literal
+let estaAprovado = true; //(true or false)boolean, ajuda a validar regras
+let sobrenome = undefined; //Undefined
+let corSelecionado = null; //Redefinir um valor
