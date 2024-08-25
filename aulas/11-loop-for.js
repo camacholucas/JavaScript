6f7e6e7546d0f5a109a04f408f6637fcfa2f0455
camacholucas <lucasdasilvacamacho@gmail.com>
2024-08-25 20:31:13 -0300

    maior valor

--- aulas/11-loop-for.js
+++ aulas/11-loop-for.js
@@ -4,8 +4,48 @@
 // 4 for in
 // 5 for of
 
-for (let i = 0; i <= 5; i++){
-    if(i % 2 !== 0){
-        console.log(i);
-    }
+// for (let i = 0; i <= 5; i++){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+
+// //While loop, você precisa declarar a variável de índice fora do loop
+
+//  let i = 10;
+
+// while(i>=1){
+    
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+//     i--;
+// }
+
+// //do while
+// let i =0;
+// do{
+//     console.log('Digitando' + i);
+//     i++
+// }while( i < 10)
+
+//for in
+const pessoa = {
+    nome: 'Lucas',
+    idade: 25
+};
+
+for(let key in pessoa){
+    console.log(key,pessoa['nome']);
+}
+
+const cores = ['a','b','c']
+
+for (let indice in cores){
+    console.log(indice,cores[indice]);
+}
+
+//for-of iteração sobre arrays
+for(let cor of cores){
+    console.log(cor);
 }
